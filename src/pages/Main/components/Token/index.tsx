c28c57c878a9bd6c24f12e5cf629a26be9a8c75e
sheriff-rango <95250818+sheriff-rango@users.noreply.github.com>
2022-07-09 00:35:39 +0800

    added token panel

--- src/pages/Main/components/Token/index.tsx
+++ src/pages/Main/components/Token/index.tsx
@@ -1,14 +1,64 @@
 import React from "react";
+import InfoCard, { InfoCardProps } from "../../../../components/InfoCard";
 
-import { Wrapper, TitleBar, MainTitle, SubTitle } from "./styled";
+import {
+  Wrapper,
+  TitleBar,
+  MainTitle,
+  SubTitle,
+  InfoContainer,
+  FooterBar,
+  FooterContent,
+  FooterBalance,
+} from "./styled";
 
 const Token: React.FC = () => {
+  const infos: InfoCardProps[] = [
+    {
+      title: "Your Allocation",
+      contents: ["100 $PUNK Available"],
+      buttonOption: {
+        title: "Stake",
+      },
+    },
+    {
+      title: "Staked $PUNK",
+      contents: ["2000 $PUNK Available"],
+      buttonOption: {
+        title: "Unstake",
+      },
+    },
+    {
+      title: "Currently Unstaking",
+      contents: ["15 Days 6 hours", "Remaining", "1000 $PUNK"],
+      buttonOption: {
+        title: "Not Claimable",
+      },
+    },
+    {
+      title: "Your Rewards",
+      contents: ["121 $PUNK Available"],
+      buttonOption: {
+        title: "Claim",
+      },
+    },
+  ];
+
   return (
     <Wrapper>
       <TitleBar>
         <MainTitle>$PUNK Token</MainTitle>
         <SubTitle>Unstaking Period 27 Days | Daily Payout 08:00 UTC</SubTitle>
       </TitleBar>
+      <InfoContainer>
+        {infos.map((info: InfoCardProps, index: number) => (
+          <InfoCard {...info} />
+        ))}
+      </InfoContainer>
+      <FooterBar>
+        <FooterContent>TOtalTokenStaked on JunoPunk.io</FooterContent>
+        <FooterBalance>145.745 $PUNK</FooterBalance>
+      </FooterBar>
     </Wrapper>
   );
 };
--- src/pages/Main/components/Token/styled.ts
+++ src/pages/Main/components/Token/styled.ts
@@ -2,13 +2,14 @@ import styled from "styled-components";
 import Card from "../../../../components/Card";
 
 export const Wrapper = styled(Card)`
-  padding: 32px 40px 0 52px;
+  padding: 32px 40px 32px 52px;
 `;
 
 export const TitleBar = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-start;
+  margin-bottom: 30px;
 `;
 
 export const MainTitle = styled.div`
@@ -20,10 +21,32 @@ export const MainTitle = styled.div`
   margin-right: 54px;
 `;
 
-export const SubTitle = styled.div`
+const NormalText = styled.div`
   color: #002cfa;
   font-style: normal;
   font-weight: 400;
   font-size: 14px;
   line-height: 16px;
+`;
+
+export const SubTitle = styled(NormalText)``;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin-bottom: 35px;
+`;
+
+export const FooterBar = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const FooterContent = styled(NormalText)``;
+
+export const FooterBalance = styled(NormalText)`
+  font-weight: bold;
+  margin-left: 24px;
 `;
